c59e7ddea6a3f6b444cadceb66f405650cb9e20d
DD <darya.dmitrievna.14.95@mail.ru>
2024-08-16 00:25:51 +0300

    EditedActionSettings

--- app/src/main/java/com/tg/let/starter/activities/SettingsUsersStarter.java
+++ app/src/main/java/com/tg/let/starter/activities/SettingsUsersStarter.java
@@ -1,6 +1,5 @@
 package com.tg.let.starter.activities;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.widget.ImageButton;
 
@@ -14,12 +13,13 @@ import com.tg.let.MainActivity;
 import com.tg.let.R;
 import com.tg.let.utils.BackButton;
 
-public class QuestionsUserStarter extends AppCompatActivity {
+public class SettingsUsersStarter extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_questions_user_starter);
+        setContentView(R.layout.activity_settings_users_starter);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -27,10 +27,6 @@ public class QuestionsUserStarter extends AppCompatActivity {
         });
 
         ImageButton backButton = findViewById(R.id.buttonBackStarter);
-        BackButton.init(backButton, MainActivity.class, true);
-
-
-        ImageButton buttonSettings = findViewById(R.id.buttonSettings);
-        BackButton.init(buttonSettings, SettingsUsersStarter.class, true);
+        BackButton.init(backButton, QuestionsUserStarter.class, true);
     }
 }
