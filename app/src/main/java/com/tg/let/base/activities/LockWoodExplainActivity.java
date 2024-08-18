858deda1cbd51d11a9a34c242d857557170abc33
tonikirillov07 <toni.kirillov.07@inbox.ru>
2024-08-18 12:21:06 +0300

    Added few base activities

--- app/src/main/java/com/tg/let/base/activities/LockWoodExplainActivity.java
+++ app/src/main/java/com/tg/let/base/activities/LockWoodExplainActivity.java
@@ -1,8 +1,6 @@
-package com.tg.let.base.activities.initialSettings;
+package com.tg.let.base.activities;
 
 import android.os.Bundle;
-import android.widget.Button;
-import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,25 +9,18 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.tg.let.R;
-import com.tg.let.utils.AnotherActivity;
-import com.tg.let.utils.BackButton;
 
-public class AccountActivityBase extends AppCompatActivity {
+public class LockWoodExplainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_account_base);
+        setContentView(R.layout.activity_lock_wood_explain);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
-
-        Button buttonNext = findViewById(R.id.buttonNext);
-        ImageButton buttonBack = findViewById(R.id.backButton);
-
-        BackButton.init(buttonBack);
     }
 }
--- app/src/main/java/com/tg/let/base/activities/initialSettings/accountSettings/AccountSettingsBaseActivity.java
+++ app/src/main/java/com/tg/let/base/activities/initialSettings/accountSettings/AccountSettingsBaseActivity.java
@@ -1,4 +1,4 @@
-package com.tg.let.base.activities.initialSettings;
+package com.tg.let.base.activities.initialSettings.accountSettings;
 
 import android.os.Bundle;
 
@@ -10,13 +10,13 @@ import androidx.core.view.WindowInsetsCompat;
 
 import com.tg.let.R;
 
-public class LookwoodHelloActivity extends AppCompatActivity {
+public class AccountSettingsBaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_lookwood_hello);
+        setContentView(R.layout.activity_account_settings_base);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
